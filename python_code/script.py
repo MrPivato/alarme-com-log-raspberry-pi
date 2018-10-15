67288eab766d55ec6e683874253457bedebedae7
MrPivato <pivatogabriel@gmail.com>
2018-10-14 22:58:38 -0300

    better code and README.md update

--- python_code/script.py
+++ python_code/script.py
@@ -1,31 +1,32 @@
 #importa modulos necessarios
-import RPi.GPIO as GPIO # da GPIO
-import time             # do tempo
-from datetime import datetime # da data
-import signal           # de eventos assincronos
-import sys              # da info sobre o interpretador
-import logging          # para criar arquivos de log
+import RPi.GPIO as GPIO         # da GPIO
+import time                     # do tempo
+from datetime import datetime   # da data
+import logging                  # para criar arquivos de log
 
 # dir e nome onde o arquivo de log sera criado
 nameLogFile = "/home/pi/uploadGDrive/log_movimentos.txt"
+
+# alcance da detectcao (use centimetros)
+detectionRange = 30.00
 
 # usar os pinos numerados
 GPIO.setmode(GPIO.BCM)
 
 # define os pinos a serem usados
-pinTrig = 14
-pinEcho = 15
-pinBuzz = 18
+trigPin = 14
+echoPin = 15
+buzzPin = 18
 
 # seta os pinos de input e output
-GPIO.setup(pinTrig, GPIO.OUT)
-GPIO.setup(pinBuzz, GPIO.OUT)
-GPIO.setup(pinEcho, GPIO.IN)
+GPIO.setup(trigPin, GPIO.OUT)
+GPIO.setup(buzzPin, GPIO.OUT)
+GPIO.setup(echoPin, GPIO.IN)
 
 # function para criar logs
 def createLog(dataHora):
-    logging.basicConfig(level = logging.INFO, filename = nameLogFile)
-    msg = dataHora.strftime("Movimento detectado %Hhrs:%Mmin:%Sseg_%d-%m-%Y")
+    logging.basicConfig(level = logging.WARNING, filename = nameLogFile)
+    msg = dataHora.strftime("Movimento detectado %Hhrs:%Mmin:%Sseg_%d-%m-%Y, " + distanceMsg)
     logging.warning(msg)
     print(msg)
 
@@ -33,31 +34,32 @@ try:
 
     while True:
 
-        GPIO.output(pinTrig, True)
+        GPIO.output(trigPin, True)
         time.sleep(0.75)
-        GPIO.output(pinTrig, False)
+        GPIO.output(trigPin, False)
 
         startTime = time.time()
         stopTime = time.time()
 
-        while 0 == GPIO.input(pinEcho):
+        while 0 == GPIO.input(echoPin):
             startTime = time.time()
 
-        while 1 == GPIO.input(pinEcho):
+        while 1 == GPIO.input(echoPin):
             stopTime = time.time()
 
         timeElapsed = stopTime - startTime # T2 - T1
         distance = (timeElapsed * 34300) / 2
 
-        print ("Distancia: %.1f cm" % distance)
+        distanceMsg = "Distancia: %.1f cm" % distance
 
-        if distance <= 30.00:
+        print (distanceMsg)
+
+        if distance <= detectionRange:
             dataHora = datetime.now()
             createLog(dataHora)
-            GPIO.output(pinBuzz, True)
+            GPIO.output(buzzPin, True)
             time.sleep(1.00)
-            GPIO.output(pinBuzz, False)
+            GPIO.output(buzzPin, False)
 
 except KeyboardInterrupt:
-    # limpa GPIO
     GPIO.cleanup()
